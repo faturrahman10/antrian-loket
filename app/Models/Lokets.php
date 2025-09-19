bdd5c0cb8f9c464469ac836fc94c33a8d039f5a0
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-19 21:16:56 +0800

    feat(loket): add currentCalled() method to get today’s called queue

--- app/Models/Lokets.php
+++ app/Models/Lokets.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lokets extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nama', ['keterangan']];
+
+    public function queues()
+    {
+        return $this->hasMany(Queue::class);
+    }
+
+    public function currentCalled()
+    {
+        return $this->queue()
+            ->where('tanggal', now()->toDateString())
+            ->whrere('status', 'dipanggil')
+            ->orderByDescending('dipanggil_pada')
+            ->first();
+    }
 }
