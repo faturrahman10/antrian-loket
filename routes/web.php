8e4a1387db052d7f56168d4fea52f0228981ea9e
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-19 19:07:07 +0800

    buat route untuk dashboar loket

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/loket', function () {
+    return view('loket.dashboard');
+})->middleware(['auth'])->name('loket.dashboard');
+
+
 require __DIR__.'/auth.php';
