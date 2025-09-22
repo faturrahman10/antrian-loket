57dd93b2314b3a61a1bf34ab6404165421a8d151
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-22 22:49:18 +0800

    add new controller: LoketController

---
+++ app/Http/Controllers/LoketController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoketController extends Controller
+{
+    public function index() {
+        $lokets = Loket::all();
+
+        return view('loket.dashboard', compact('lokets'));
+    }
+}
