52a0ea200bf1e2c179e94355296e40467271e142
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-27 21:05:55 +0800

    membuat method store

--- app/Http/Controllers/LoketController.php
+++ app/Http/Controllers/LoketController.php
@@ -7,13 +7,26 @@ use Illuminate\Http\Request;
 
 class LoketController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $lokets = Loket::all();
-        
         return view('loket.dashboard', compact('lokets'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('loket.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'nama' => 'required|string|max:255',
+            'keterangan => required|string|max:255',
+        ]);
+
+        Loket::create($validate);
+
+        return redirect()->route('loket.dashboard')->with('success', 'Loket berhasil ditambahkan');
     }
 }
