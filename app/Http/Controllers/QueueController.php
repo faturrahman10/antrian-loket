2de425e34d797f42c70b8b08e4b09d2958070333
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-28 17:03:32 +0800

    create function index for queue

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Loket;
 use Illuminate\Http\Request;
 
 class QueueController extends Controller
 {
-    //
+    public function index()
+    {
+        $lokets = Loket::with(['queues' => function($q){
+            $q->where('tanggal', now()->toDateString())->orderBy('nomor');
+        }])->get();
+
+        return view('queue.index', compact('lokets'));
+    }
 }
