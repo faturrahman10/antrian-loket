f80b3374f24a93b4095d029f26d0ae296148994c
faturrahman10 <faturrahmanhasir@gmail.com>
2025-09-28 11:18:07 +0800

    add button tambah loket

--- resources/views/loket/dashboard.blade.php
+++ resources/views/loket/dashboard.blade.php
@@ -9,6 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <h3 class="text-lg font-medium mb-4">Daftar Loket</h3>
+                <div class="mb-4">
+                    <a href="{{ route('loket.create') }}" class="inline-block bg-green-500 text-white px-3 py-1 rounded">
+                        + Tambah Loket
+                    </a>
+                </div>
+
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead>
                         <tr>
