4c60eebcf097caaeeebdbc4b57d3c6c8fb81befc
faturrahman10 <faturrahmanhasir@gmail.com>
2025-10-06 14:52:43 +0800

    update method store, show, take, finish, and skip

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -6,6 +6,7 @@ use App\Models\Loket;
 use App\Models\Queue;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class QueueController extends Controller
 {
@@ -17,20 +18,22 @@ class QueueController extends Controller
         return view('queue.dashboard', compact('queues'));
     }
 
-    public function store(Loket $loket)
+    public function store()
     {
-        $today = Carbon::today();
-        $last = Queue::whereDate('tanggal', $today)->orderByDesc('nomor')->first();
-        $nextNumber = $last ? $last->nomor + 1 : 1;
+        $today = now()->toDateString();
+        return DB::transaction(function () use ($today) {
+            $last = Queue::whereDate('tanggal', $today)->orderByDesc('nomor')->first();
+            $nextNumber = ($last ? $last->nomor : 0) + 1;
 
-        Queue::create([
-            'nomor' => $nextNumber,
-            'status' => 'menunggu',
-            'tanggal' => $today,
-            'loket_id' => null,
-        ]);
-
-        return redirect()->route('queue.dashboard')->with('success', 'nomor antrian baru ditambahkan');
+            Queue::create([
+                'tanggal' => $today,
+                'nomor' => $nextNumber,
+                'status' => 'menunggu',
+            ]);
+            return redirect()
+                ->back()
+                ->with('success', 'Antrian #' . $nextNumber . ' berhasil dibuat');
+        });
     }
 
     public function show(Loket $loket)
@@ -39,7 +42,7 @@ class QueueController extends Controller
 
         $queues = Queue::where('loket_id', $loket->id)->whereDate('tanggal', $today)->orderBy('nomor')->get();
 
-        return view('queueu.index', compact('loket', 'queues'));
+        return view('queue.index', compact('loket', 'queues'));
     }
 
     public function take(Loket $loket)
@@ -47,27 +50,27 @@ class QueueController extends Controller
         $today = now()->toDateString();
         $queue = Queue::whereNull('loket_id')->whereDate('tanggal', $today)->orderBy('nomor')->first();
 
-        if(!$queue) {
+        if (!$queue) {
             return back()->with('error', 'Tidak ada antrian menunggu');
         }
-        $queue->upddate([
+        $queue->update([
             'loket_id' => $loket->id,
             'status' => 'dipanggil',
             'dipanggil_pada' => now(),
         ]);
 
-        return back()->with('success', 'antrian #'.$queue->nomor.' sedang dipanggil di loket '.$loket->nama);
+        return back()->with('success', 'antrian #' . $queue->nomor . ' sedang dipanggil di loket ' . $loket->nama);
     }
 
     public function finish(Queue $queue)
     {
         $queue->update(['status' => 'selesai']);
-        return back()->with('success', 'Antrian #'.$queue->nomor.' selesai');
+        return back()->with('success', 'Antrian #' . $queue->nomor . ' selesai');
     }
 
     public function skip(Queue $queue)
     {
         $queue->update(['status' => 'dilewati']);
-        return back()->with('success', 'Antrian #'.$queue->nomor.' dilewati');
+        return back()->with('success', 'Antrian #' . $queue->nomor . ' dilewati');
     }
 }
