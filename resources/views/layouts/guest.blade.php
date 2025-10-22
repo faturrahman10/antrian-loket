c1383d1fa9c6a4c8ec3648a43a993f0d8ea234ff
faturrahman10 <faturrahmanhasir@gmail.com>
2025-10-22 21:47:38 +0800

    add width and height for logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo/>
+                    <x-application-logo class="w-w-36 h-16"/>
                 </a>
             </div>
 
